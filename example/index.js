f1c14b9b04ffebb75427ea7106713375051d4c14
Greg Allen <me@jga.me>
2015-01-27 21:29:37 -0800

    set up hapi-redirects

--- example/index.js
+++ example/index.js
@@ -1,5 +1,13 @@
 var Rapptor = require('../');
 
 var rapptor = new Rapptor();
+
+rapptor.server.route({
+  method: 'GET',
+  path: '/', 
+  handler: function(request, reply) {
+    reply('yep');
+  }
+});
 rapptor.start();
 
