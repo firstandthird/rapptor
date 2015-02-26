af4db7b599fb04ed56b7da16c195a16763867809
Eric Willis <eric@ecwillis.com>
2015-02-26 10:47:21 -0800

    Reordering arguments after second guessing myself

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ Rapptor.prototype._setupViews = function() {
       context.env = config.env;
 
       if (typeof this.getViewData === 'function') {
-        this.getViewData(request, config, context);
+        this.getViewData(request, context, config);
       }
       response.source.context = context;
 
