d5b7a4e83399012e8727e6d8248082cda3f4f97e
Greg Allen <me@jga.me>
2015-02-27 11:38:23 -0800

    generalized asset loading

--- methods/assets.js
+++ methods/assets.js
@@ -3,20 +3,17 @@ var path = require('path');
 var assets = {};
 
 module.exports = function() {
-  var assetPath = path.join(this.cwd, this.config.structure.assets, '_prod/assets.json');
-  if (fs.existsSync(assetPath)) {
-    assets = require(assetPath);
+  if (this.config.assets.mapping) {
+    var assetPath = path.join(this.cwd, this.config.structure.assets, this.config.assets.mapping);
+    if (fs.existsSync(assetPath)) {
+      assets = require(assetPath);
+    }
   }
 
   return function(basename, type) {
-    var useHash = (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'stage' || process.env.NODE_ENV == 'qa');
-
     var filename = basename + '.' + type;
 
-    var hashFile = assets[filename];
-    if (useHash && hashFile) {
-      return '_prod/' + hashFile;
-    }
-    return '_dist/' + filename;
+    var assetFile = assets[filename] || filename;
+    return path.join(this.config.assets.compiled, assetFile);
   }.bind(this);
 };
