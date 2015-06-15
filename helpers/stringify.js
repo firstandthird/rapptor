053d1da2b05b4326453cf84338cc2f974b128dfe
Greg Allen <me@jga.me>
2015-06-15 12:31:03 -0700

    fixed a bug where stringify would have a circular reference because _server is getting added

--- helpers/stringify.js
+++ helpers/stringify.js
@@ -1,5 +1,8 @@
 var Handlebars = require('handlebars');
 module.exports = function(input, pretty) {
   var spaces = (pretty) ? '  ' : '';
+  if (input._server) {
+    delete input._server;
+  }
   return new Handlebars.SafeString(JSON.stringify(input, null, spaces));
 };
