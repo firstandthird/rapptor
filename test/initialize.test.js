25b28a219459bd6e7ce62050e9ccb4b2c04334d0
chris <orthagonal1@gmail.com>
2017-02-09 09:14:34 -0600

    in progress

--- test/initialize.test.js
+++ test/initialize.test.js
@@ -6,11 +6,16 @@ const lab = exports.lab = Lab.script();
 
 lab.experiment('Rapptor#initialization', () => {
   lab.test('initializes a new instance of rapptor', (done) => {
-    const rapptor = new Rapptor();
+    const rapptor = new Rapptor({
+      CWD: __dirname
+    });
     Code.expect(typeof rapptor).to.equal('object');
-    console.log(Object.keys(rapptor))
-    console.log(rapptor.options)
-    Code.expect(typeof rapptor.server).to.equal('object');
+    console.log(Object.keys(rapptor));
+    console.log(rapptor.options);
+    rapptor.start((err, server, config) => {
+      Code.expect(err).to.equal(null);
+      Code.expect(typeof server).to.equal('object');
+    });
     done();
   });
 });
