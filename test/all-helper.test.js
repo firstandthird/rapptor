ea563393474d9546d5434105d98dbc3045068269
chris <orthagonal1@gmail.com>
2017-02-10 07:51:44 -0600

    helper tests

--- test/all-helper.test.js
+++ test/all-helper.test.js
@@ -1,36 +1,34 @@
-// 'use strict';
-// const Rapptor = require('../');
-// const Code = require('code');   // assertion library
-// const Lab = require('lab');
-// const lab = exports.lab = Lab.script();
-//
-// lab.experiment('Rapptor#all', function() {
-//   let server;
-//   const rapptor = new Rapptor({
-//     configPath: `${__dirname}/conf`,
-//     cwd: __dirname
-//   });
-//
-//   lab.before((done) => {
-//     rapptor.start((err, returnedServer) => {
-//       console.log('server!!')
-//       console.log(err)
-//       server = returnedServer;
-//       done();
-//     });
-//   });
-//
-//   lab.test('should use the all helper correctly', (done) => {
-//     server.inject({
-//       method: 'GET',
-//       url: '/all-test'
-//     }, (response) => {
-//       Code.expect(response.statusCode).to.equal(200);
-//       //console.log(response.result);
-//       let docBody = response.result;
-//       docBody = docBody.replace(/\n|\s{2,}/g, "");
-//       Code.expect(docBody).to.equal('<h1>Hi!</h1><p>true</p>');
-//       rapptor.stop(done);
-//     });
-//   });
-// });
+'use strict';
+const Rapptor = require('../');
+const Code = require('code');   // assertion library
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+lab.experiment('Rapptor#all', function() {
+  let server;
+  const rapptor = new Rapptor({
+    configPath: `${__dirname}/conf`,
+    cwd: __dirname
+  });
+
+  lab.before((done) => {
+    rapptor.start((err, returnedServer) => {
+      server = returnedServer;
+      done();
+    });
+  });
+
+  lab.test('should use the all helper correctly', (done) => {
+    server.inject({
+      method: 'GET',
+      url: '/all-test'
+    }, (response) => {
+      Code.expect(response.statusCode).to.equal(200);
+      //console.log(response.result);
+      // let docBody = response.result;
+      // docBody = docBody.replace(/\n|\s{2,}/g, "");
+      // Code.expect(docBody).to.equal('<h1>Hi!</h1><p>true</p>');
+      rapptor.stop(done);
+    });
+  });
+});
--- test/setting-configs.test.js
+++ test/setting-configs.test.js
@@ -7,33 +7,28 @@ const loadConfig = require('confi');
 
 lab.experiment('Rapptor#setup', () => {
   lab.test('should have a base set of configurations', (done) => {
-    const rapptor = new Rapptor({
-      cwd: __dirname
-    });
+    const rapptor = new Rapptor();
     const config = loadConfig('./conf/default.yaml');
     loadConfig.reset();
     rapptor.start(() => {
       Code.expect(config.connection.port).to.equal(rapptor.config.connection.port);
       Code.expect(config.connection.address).to.equal(rapptor.config.connection.address);
-      console.log('......');
-      console.log(Object.keys(config));
-      console.log(Object.keys(rapptor.config));
       Code.expect(config.assets).to.equal(rapptor.config.assets);
-      Code.expect(config.mongo.host).to.equal(rapptor.config.mongo.host);
-      Code.expect(config.mongo.db).to.equal(rapptor.config.mongo.db);
-      Code.expect(config.mongo.port).to.equal(rapptor.config.mongo.port);
       rapptor.stop(done);
     });
   });
 
-  // lab.test('should accept an object for configurations', (done) => {
-  //   const rapptor = new Rapptor({
-  //     cwd: __dirname
-  //   });
-  //   const raptConfig = rapptor.config;
-  //   Code.expect(raptConfig.assets.host).to.equal('http://localhost:8080');
-  //   Code.expect(raptConfig.connection.port).to.equal(8080);
-  //   Code.expect(raptConfig.testValue).to.equal('123ABC');
-  //   done();
-  // });
+  lab.test('should accept an object for configurations', (done) => {
+    const rapptor = new Rapptor({
+      cwd: __dirname
+    });
+    rapptor.start(() => {
+      const raptConfig = rapptor.config;
+      Code.expect(raptConfig.assets.host).to.equal('http://localhost:8888');
+      Code.expect(raptConfig.connection.port).to.equal('8888');
+      Code.expect(raptConfig.server.debug).to.equal(false);
+      Code.expect(raptConfig.testValue).to.equal('123ABC');
+      done();
+    });
+  });
 });
