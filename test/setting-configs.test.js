7cfbfed63d9f9aded6fcbde863190b73a713b48d
chris <orthagonal1@gmail.com>
2017-02-12 22:06:46 -0600

    update settings-config

--- test/setting-configs.test.js
+++ test/setting-configs.test.js
@@ -5,26 +5,50 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 
 lab.experiment('Rapptor#setup', () => {
-  lab.test('should have a base set of configurations', (done) => {
+  lab.test('default options', (done) => {
     const rapptor = new Rapptor();
-    rapptor.start(() => {
-      Code.expect(rapptor.config.connection.port).to.equal(1234);
-      Code.expect(rapptor.config.connection.address).to.equal('0.0.0.0');
-      rapptor.stop(done);
+    rapptor.start((err) => {
+      // should fail because hapi-method-loader cannot find 'methods' directory in root:
+      Code.expect(err.toString()).to.include('ENOENT: no such file or directory');
+      done();
     });
   });
 
-  lab.test('should accept an object for configurations', (done) => {
+  lab.test('accept a cwd', (done) => {
     const rapptor = new Rapptor({
-      cwd: __dirname,
-      configPath: `${__dirname}/conf`
+      cwd: __dirname
     });
     rapptor.start(() => {
       const raptConfig = rapptor.config;
+      // should load config from tests/conf
       Code.expect(raptConfig.assets.host).to.equal('http://localhost:8080');
       Code.expect(raptConfig.connection.port).to.equal(8080);
       Code.expect(raptConfig.server.debug).to.equal(false);
       Code.expect(raptConfig.testValue).to.equal('123ABC');
+      done();
+    });
+  });
+
+  lab.test('accept a configPath', (done) => {
+    const rapptor = new Rapptor({
+      configPath: `${__dirname}/conf2`
+    });
+    rapptor.start(() => {
+      const raptConfig = rapptor.config;
+      Code.expect(raptConfig.connection.port).to.equal('8080');
+      Code.expect(raptConfig.testValue).to.equal('conf2!');
+      done();
+    });
+  });
+
+  lab.test('both cwd and configPath', (done) => {
+    const rapptor = new Rapptor({
+      cwd: __dirname,
+      configPath: `${__dirname}/conf2`
+    });
+    rapptor.start(() => {
+      const raptConfig = rapptor.config;
+      Code.expect(raptConfig.testValue).to.equal('conf2!');
       done();
     });
   });
