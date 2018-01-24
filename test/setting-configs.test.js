a8dc248df637859d1fbdb14028366ca60d870e60
Chris <orthagonal1@gmail.com>
2018-01-23 19:34:52 -0600

    clarify comment

--- test/setting-configs.test.js
+++ test/setting-configs.test.js
@@ -68,6 +68,6 @@ lab.experiment('Rapptor#setup', () => {
       Code.expect(e.toString().startsWith('YAMLException:')).to.equal(true);
       return;
     }
-    throw new Error('rapptor should throw error');
+    throw new Error('rapptor should throw YAML error');
   });
 });
