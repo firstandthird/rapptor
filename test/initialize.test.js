ba2c2fd702512ccc01a0ea124836f711be77f8a9
Chris <orthagonal1@gmail.com>
2018-03-02 11:16:30 -0600

    tapify

--- test/initialize.test.js
+++ test/initialize.test.js
@@ -1,42 +1,36 @@
 'use strict';
 const Rapptor = require('../');
-const Code = require('code'); // assertion library
-const Lab = require('lab');
-const lab = exports.lab = Lab.script();
+const tap = require('tap');
 
-lab.experiment('Rapptor#initialization', { timeout: 5000 }, () => {
-  lab.test('initializes a new instance of rapptor', async() => {
-    const rapptor = new Rapptor({
-      cwd: __dirname,
-    });
-    Code.expect(typeof rapptor).to.equal('object');
-    Code.expect(typeof rapptor.start).to.equal('function');
-    Code.expect(typeof rapptor.setup).to.equal('function');
-    const { server, config } = await rapptor.start();
-    Code.expect(typeof server).to.equal('object');
-    Code.expect(typeof config).to.equal('object');
-    await rapptor.stop();
+tap.test('initializes a new instance of rapptor', async t => {
+  const rapptor = new Rapptor({
+    cwd: __dirname,
   });
+  t.equal(typeof rapptor, 'object');
+  t.equal(typeof rapptor.start, 'function');
+  t.equal(typeof rapptor.setup, 'function');
+  const { server, config } = await rapptor.start();
+  t.equal(typeof server, 'object');
+  t.equal(typeof config, 'object');
+  await rapptor.stop();
+});
 
-  lab.test('server stops when SIGTERM event is emitted ', async() => {
-    const rapptor = new Rapptor({
-      cwd: __dirname,
-    });
-    await rapptor.start();
-    process.emit('SIGTERM');
-    // wait for server to wind down, tests will crash if it does not:
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    Code.expect(process.listenerCount('SIGTERM')).to.equal(0);
+tap.test('can config a new instance of rapptor without starting the server', async t => {
+  const rapptor = new Rapptor({
+    cwd: __dirname,
   });
+  const { server, config } = await rapptor.setup();
+  t.equal(typeof server, 'object');
+  t.equal(typeof config, 'object');
+  // verify server isn't started yet:
+  t.equal(server.info.started, 0);
+});
 
-  lab.test('can config a new instance of rapptor without starting the server', async() => {
-    const rapptor = new Rapptor({
-      cwd: __dirname,
-    });
-    const { server, config } = await rapptor.setup();
-    Code.expect(typeof server).to.equal('object');
-    Code.expect(typeof config).to.equal('object');
-    // verify server isn't started yet:
-    Code.expect(server.info.started).to.equal(0);
+tap.test('server stops when SIGTERM event is emitted ', async t => {
+  const rapptor = new Rapptor({
+    cwd: __dirname,
   });
+  await rapptor.start();
+  // server will stop and test will exit:
+  process.emit('SIGTERM');
 });
