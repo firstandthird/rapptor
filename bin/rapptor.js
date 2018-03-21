c207edf29e0ac8996ea880932aa0a0bfb5864df5
orthagonal <orthagonal1@gmail.com>
2018-03-21 10:58:11 -0500

    config option (#157)
    * config option
    
    * add eslint disable
    
    * don't override cwd
    
    * simplify

--- bin/rapptor.js
+++ bin/rapptor.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 'use strict';
 const Rapptor = require('../');
-const path = require('path');
 
-let cwd = process.cwd();
-if (process.argv.length === 3) {
-  cwd = path.resolve(cwd, process.argv[2]);
-}
+const cwd = process.cwd();
+
 const main = async function() {
   const rapptor = new Rapptor({
     configPath: `${cwd}/conf`,
     cwd
   });
-
   try {
-    await rapptor.start();
+    // running "rapptor config" will just show the server configuration without actually launching it:
+    if (process.argv.length >= 3 && process.argv[2] === 'config') {
+      const { config } = await rapptor.setup();
+      console.log(JSON.stringify(config, null, 2)); //eslint-disable-line no-console
+    } else {
+      await rapptor.start();
+    }
   } catch (e) {
     console.error(e.stack || e.message); //eslint-disable-line no-console
     process.exit(1);
