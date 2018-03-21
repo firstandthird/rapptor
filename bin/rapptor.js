4fff7ded3abe53f94b731e604eb71b3c75d91133
Chris <orthagonal1@gmail.com>
2018-03-21 09:24:34 -0500

    config option

--- bin/rapptor.js
+++ bin/rapptor.js
@@ -4,17 +4,33 @@ const Rapptor = require('../');
 const path = require('path');
 
 let cwd = process.cwd();
+let justConfig = false;
+
 if (process.argv.length === 3) {
-  cwd = path.resolve(cwd, process.argv[2]);
+  if (process.argv[2] === 'config') {
+    justConfig = true;
+  } else {
+    cwd = path.resolve(cwd, process.argv[2]);
+  }
+} else if (process.argv.length === 4) {
+  if (process.argv[2] === 'config') {
+    justConfig = true;
+    cwd = path.resolve(cwd, process.argv[3]);
+  }
 }
+
 const main = async function() {
   const rapptor = new Rapptor({
     configPath: `${cwd}/conf`,
     cwd
   });
-
   try {
-    await rapptor.start();
+    if (justConfig) {
+      const { config } = await rapptor.setup();
+      console.log(JSON.stringify(config, null, 2));
+    } else {
+      await rapptor.start();
+    }
   } catch (e) {
     console.error(e.stack || e.message); //eslint-disable-line no-console
     process.exit(1);
