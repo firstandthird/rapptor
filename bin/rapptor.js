fcc88fa605f1ac7d0db7c470f642b0388b46c220
Chris <orthagonal1@gmail.com>
2018-03-21 10:50:09 -0500

    simplify

--- bin/rapptor.js
+++ bin/rapptor.js
@@ -3,11 +3,6 @@
 const Rapptor = require('../');
 
 const cwd = process.cwd();
-let justConfig = false;
-
-if (process.argv.length >= 3 && process.argv[2] === 'config') {
-  justConfig = true;
-}
 
 const main = async function() {
   const rapptor = new Rapptor({
@@ -15,7 +10,8 @@ const main = async function() {
     cwd
   });
   try {
-    if (justConfig) {
+    // running "rapptor config" will just show the server configuration without actually launching it:
+    if (process.argv.length >= 3 && process.argv[2] === 'config') {
       const { config } = await rapptor.setup();
       console.log(JSON.stringify(config, null, 2)); //eslint-disable-line no-console
     } else {
