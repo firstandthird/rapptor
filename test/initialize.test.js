92a8cb64dbf7583cdab80b8c8d35c842d31c14ed
orthagonal <orthagonal1@gmail.com>
2018-09-12 03:34:27 +0000

    test _enabled plugins

--- test/initialize.test.js
+++ test/initialize.test.js
@@ -68,3 +68,50 @@ tap.test('server stops when SIGTERM event is emitted ', async t => {
   const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
   await wait(2000);
 });
+
+tap.test('be able to conditionally load hapi-timing plugin', async t => {
+  process.env.TIMING = true;
+  const rapptor = new Rapptor({
+    cwd: __dirname,
+  });
+  const { server, config } = await rapptor.setup();
+  t.notEqual(config.plugins['hapi-timing']._enabled, false, 'loads hapi-timing');
+  t.equal(server.events._eventListeners.response.handlers.length, 2, 'registers hapi-timing event handler');
+  await rapptor.stop();
+  t.end();
+});
+
+tap.test('be able to conditionally load hapi-cache-stats plugin', async t => {
+  process.env.CACHE_STATS = true;
+  const rapptor = new Rapptor({
+    cwd: __dirname,
+  });
+  const { server, config } = await rapptor.setup();
+  t.notEqual(config.plugins['hapi-cache-stats']._enabled, false, 'loads hapi-cache-stats');
+  t.equal(server.events._eventListeners.response.handlers.length, 2, 'registers hapi-cache-stats event handler');
+  await rapptor.stop();
+  t.end();
+});
+
+tap.test('be able to conditionally load hapi-require-https plugin', async t => {
+  process.env.FORCE_HTTPS = true;
+  const rapptor = new Rapptor({
+    cwd: __dirname,
+  });
+  const { config } = await rapptor.setup();
+  t.notEqual(config.plugins['hapi-require-https']._enabled, false, 'loads hapi-require-https');
+  await rapptor.stop();
+  t.end();
+});
+
+tap.test('be able to conditionally load hapi-oppsy plugin', async t => {
+  process.env.OPS_INTERVAL = 5;
+  const rapptor = new Rapptor({
+    cwd: __dirname,
+  });
+  const { server, config } = await rapptor.setup();
+  t.notEqual(config.plugins['hapi-oppsy']._enabled, false, 'loads hapi-oppsy');
+  t.equal(server.events._eventListeners.stop.handlers.length, 2, 'registers hapi-oppsy event handler');
+  await rapptor.stop();
+  t.end();
+});
