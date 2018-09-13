7d7a5eddd299cc7b968f7355defc20fe90a1d748
orthagonal <orthagonal1@gmail.com>
2018-09-13 01:27:36 +0000

    check moar plugins

--- test/expectedOutput.js
+++ test/expectedOutput.js
@@ -33,6 +33,34 @@ module.exports = {
       token: false,
       endpoint: '/health',
       auth: false
+    },
+    'hapi-trailing-slash': {
+      method: 'remove',
+      statusCode: 301
+    },
+    'hapi-logr': {
+      unhandledRejection: true,
+      uncaughtException: true
+    },
+    'hapi-timing': { _enabled: false, threshold: 300 },
+    'hapi-cache-stats': { interval: false, _enabled: false, verbose: false },
+    'hapi-oppsy': { _enabled: false, interval: 0, logCpu: false, logMemory: false },
+    'hapi-require-https': { _enabled: false },
+    'hapi-method-loader': {
+      cwd: '/home/ubuntu/Documents/GitHub/rapptor',
+      verbose: false,
+      path: '/home/ubuntu/Documents/GitHub/rapptor/methods',
+      autoLoad: true
+    },
+    'hapi-route-loader': {
+      cwd: '/home/ubuntu/Documents/GitHub/rapptor',
+      prefix: '',
+      verbose: false
+    },
+    'hapi-log-response': {
+      requests: false,
+      includeEventTags: true,
+      ignoreUnauthorizedTry: true,
     }
   }
 };
